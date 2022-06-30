ba66364fdb40de357aa6b7efc0b1a71407ec0ee1
Loba <75981179+L0oba@users.noreply.github.com>
2022-06-30 13:43:24 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,15 +2,10 @@ let acl = new Accelerometer({frequency: 60});
 console.log("Beginnig test");
 acl.addEventListener('reading', () => {
 //make item with the id box move 
-
 let x = acl.x * 100;
 let y = acl.y * 100;
 let z = acl.z * 100;
-
-// console.log('x: ' + x);
-// console.log('y: ' + y);
-// console.log('z: ' + z);
-
+	
 let box = document.getElementById('box1');
 box.style.left = x + 'px';
 box.style.top = y + 'px';
@@ -20,22 +15,7 @@ acl.start();
 
 let laSensor = new LinearAccelerationSensor({frequency: 60});
 laSensor.addEventListener('reading', e => {
-	// console.log("Linear acceleration along the X-axis " + laSensor.x);
-	// console.log("Linear acceleration along the Y-axis " + laSensor.y);
-	// console.log("Linear acceleration along the Z-axis " + laSensor.z);
-  
 	let box = document.getElementById('box2');
 	box.style.left = (laSensor.x * 100) + 'px';
 	box.style.top = (laSensor.y * 100) + 'px';
   });
-  laSensor.start();
-
-const sensor = new AmbientLightSensor();
-sensor.addEventListener('reading', event => {
-	console.log('Current light level:', sensor.illuminance);
-});
-sensor.addEventListener('error', event => {
-	console.log(event.error.name, event.error.message);
-});
-sensor.start();
-  
